a7469202b2d0741bead3f7e938fd8eb938135955
Youcef Megoura <meg.youcef@yahoo.fr>
2023-02-18 19:51:51 +0100

    Fix: name of web application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'presenceQR-webclient-client';
+  title = 'presenceQR-webAdmin-client';
 }
